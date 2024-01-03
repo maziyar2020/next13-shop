95c5fef4f0cfcb6c4340c8ff2064f2638bba280b
maziyar <maziroadtodivine@gmail.com>
2024-01-03 22:15:52 +0330

    Project initial commit . install dependecies and set global fonts and colors.

---
+++ src/utils/toLocalDate.js
@@ -0,0 +1,13 @@
+export function toLocalDateString(date) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString("fa-IR", options);
+}
+
+export function toLocalDateStringShort(date) {
+  return new Date(date).toLocaleDateString("fa-IR");
+}

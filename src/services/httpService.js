9793348799784600cf55053489fbc6752e155577
maziyar <maziroadtodivine@gmail.com>
2024-01-17 19:04:09 +0330

    Create interceptors for refresh token .

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,8 +3,38 @@ import axios from 'axios'
 const app = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
-    
+
 })
+
+app.interceptors.request.use(
+    res => res,
+    err => Promise.reject(err)
+)
+
+app.interceptors.response.use(
+    res => res,
+    async (err) => {
+        const originalConfig = err.config
+        if (err.response.status === 401) {
+            console.log('hi', err);
+        }
+        // if condition failed return promise
+        if (err.response.status === 401 && !originalConfig._retry) {
+            originalConfig._retry = true;
+            try {
+                const { data } = await axios.get(
+                    `${process.env.NEXT_PUBLIC_API_URL}/user/refresh-token`,
+                    { withCredentials: true }
+                )
+                if (data) return app(originalConfig)
+            } catch (error) {
+                return Promise.reject(error)
+            }
+        }
+        return Promise.reject(err)
+    }
+
+)
 
 
 const http = {
@@ -12,7 +42,7 @@ const http = {
     post: app.post,
     delete: app.delete,
     put: app.put,
-    patch : app.patch
+    patch: app.patch
 }
 
 export default http
